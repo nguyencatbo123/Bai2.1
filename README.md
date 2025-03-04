35f94793d19ed5569cad30708170cdd22b01ba1e
nguyencatbo <nguyencatbo123@gmail.com>
2025-03-04 10:48:30 +0700

    Nop Bai

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Bai2.1
+# Nguyen Thanh Dat
+### MSV: 22810310314
+## Custom component
+## Ket qua
+
+![anh1](anh1.jpg)
---
+++ my-app/App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.square}>
+        <Text style={styles.text}>Hello, world</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  square: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+  },
+});
